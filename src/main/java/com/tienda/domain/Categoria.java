1d3e34716b9cdefe0eba95f376b60e2590d6afdb
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-07-01 21:03:39 -0600

    commit de la semana 6

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.tienda.domain;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+@Data //especificar que es una capa de datos 
+@Entity //Esta clase esta enlazada a una tabla 
+@Table (name="categoria") //Especificar cual entidad/tabla
+public class Categoria implements Serializable {
+    //Basicamente esto es un AUTO INCREMENT
+    private static final long serialVersionUID = 1L; 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_categoria")
+
+    private Long idCategoria;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+
+public Categoria() {
+
+}    
+ 
+public Categoria(String categoria, boolean activo) {
+    this.descripcion = categoria;
+    this.activo = activo;
+}
+
+}
---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.tienda.service;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+//El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-373a4.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-373a4-firebase-adminsdk-e6i62-bbd571e232.json";
+
+}
